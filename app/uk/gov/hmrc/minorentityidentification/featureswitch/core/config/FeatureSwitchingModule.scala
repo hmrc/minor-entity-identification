6e855085b8feba6b5b8d382f924100075324a3d7
Tadas Vaidotas <1133367+tvaidotas@users.noreply.github.com>
2022-03-02 15:08:11 +0000

    SAR-9255: adding FS for the getCTReference (#11)
    SAR-9255: adding FS for the getCTReference (#11)

--- app/uk/gov/hmrc/minorentityidentification/featureswitch/core/config/FeatureSwitchingModule.scala
+++ app/uk/gov/hmrc/minorentityidentification/featureswitch/core/config/FeatureSwitchingModule.scala
@@ -25,20 +25,22 @@ import javax.inject.Singleton
 @Singleton
 class FeatureSwitchingModule extends Module with FeatureSwitchRegistry {
 
-  val switches = Seq(DesStub)
+  val switches = Seq(DesStub, StubGetCtReference)
 
   override def bindings(environment: Environment, configuration: Configuration): Seq[Binding[_]] = {
     Seq(
       bind[FeatureSwitchRegistry].to(this).eagerly()
     )
   }
+
+  case object DesStub extends FeatureSwitch {
+    override val configName: String = "feature-switch.des-stub"
+    override val displayName: String = "Use stub for submissions to Registration"
+  }
+
+  case object StubGetCtReference extends FeatureSwitch {
+    override val configName: String = "feature-switch.ct-reference-stub"
+    override val displayName: String = "Use stub for Get CT Reference"
+  }
+
 }
-
-case object DesStub extends FeatureSwitch {
-  override val configName: String = "feature-switch.des-stub"
-  override val displayName: String = "Use stub for submissions to Registration"
-}
-
-
-
-
